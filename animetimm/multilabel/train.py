a48bfe21615833d95d6fa981f686314e3e1b75a4
narugo1992 <narugo1992@deepghs.org>
2025-05-08 14:04:25 +0800

    dev(narugo): add pretrained_tag

--- animetimm/multilabel/train.py
+++ animetimm/multilabel/train.py
@@ -404,10 +404,11 @@ if __name__ == '__main__':
     logging.try_init_root(logging.INFO)
     set_name = str(os.environ.get('S', '150k') or '150k')
     max_epoch = int(os.environ.get('MEP', '100') or '100')
+    mn = str(os.environ.get('M', 'caformer_s36.sail_in22k_ft_in1k_384') or 'caformer_s36.sail_in22k_ft_in1k_384')
     train(
-        workdir=f'runs/tiny_experiments_{set_name}_p512x_d0.4_mep{max_epoch}',
+        workdir=f'runs/{mn}_{set_name}_p512x_d0.4_mep{max_epoch}',
         dataset_repo_id=f'animetimm/danbooru-wdtagger-v4-w640-ws-{set_name}',
-        timm_model_name='caformer_s36.sail_in22k_ft_in1k_384',
+        timm_model_name=mn,
         num_workers=32,
         batch_size=64,
         learning_rate=2e-4,
