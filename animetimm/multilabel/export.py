c558c34733536b08706c2d4888c6f3e4605764ec
narugo1992 <narugo1992@deepghs.org>
2025-05-13 01:14:35 +0800

    dev(narugo): add profiler

--- animetimm/multilabel/export.py
+++ animetimm/multilabel/export.py
@@ -70,7 +70,7 @@ def export(workdir: str, repo_id: Optional[str] = None, private: bool = False, l
                 with open(os.path.join(workdir, 'test_metrics.json'), 'r') as mf:
                     metrics_info['test'] = json.load(mf)
             metrics_info['val'] = {
-                key.split('/', maxsplit=1)[1:]: value
+                key.split('/', maxsplit=1)[-1]: value
                 for key, value in metrics.items()
                 if isinstance(value, (type(None), int, float, str))
             }
