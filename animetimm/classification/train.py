8fd7ae477b5caee3656c4074018de7f1ddaef47e
narugo1992 <narugo1992@deepghs.org>
2025-05-16 01:11:41 +0800

    dev(narugo): add x

--- animetimm/classification/train.py
+++ animetimm/classification/train.py
@@ -243,7 +243,9 @@ def train(
             outputs = module(inputs)
             train_total += labels_.shape[0]
 
-            print(('input', inputs.shape, inputs.dtype, 'labels', labels_.shape, labels_.dtype, 'outputs', outputs.shape, outputs.dtype))
+            print((
+                  'input', inputs.shape, inputs.dtype, 'labels', labels_.shape, labels_.dtype, 'outputs', outputs.shape,
+                  outputs.dtype, labels_))
 
             with torch.no_grad():
                 as_ = torch.argsort(outputs, dim=-1)
