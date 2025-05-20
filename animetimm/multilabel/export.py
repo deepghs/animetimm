1e1e0b143355fed21b4d178159844eb671a32423
narugo1992 <narugo1992@deepghs.org>
2025-05-20 15:23:56 +0800

    dev(narugo): add tensorboard file check

--- animetimm/multilabel/export.py
+++ animetimm/multilabel/export.py
@@ -26,7 +26,7 @@ from .test import test
 from ..dataset import load_pretrained_tag
 from ..model import Model
 from ..onnx import export_model_to_onnx
-from ..utils import torch_model_profile, GLOBAL_CONTEXT_SETTINGS, print_version
+from ..utils import torch_model_profile, GLOBAL_CONTEXT_SETTINGS, print_version, is_tensorboard_log_empty
 
 _LOG_FILE_PATTERN = re.compile(r'^events\.out\.tfevents\.(?P<timestamp>\d+)\.(?P<machine>[^.]+)\.(?P<extra>[\s\S]+)$')
 
@@ -189,6 +189,10 @@ def export(workdir: str, repo_id: Optional[str] = None,
 
         for logfile in glob.glob(os.path.join(workdir, 'events.out.tfevents.*')):
             logging.info(f'Tensorboard file {logfile!r} found.')
+            if is_tensorboard_log_empty(logfile):
+                logging.warning(f'Tensorboard file {logfile!r} is empty, skipped.')
+                continue
+
             matching = _LOG_FILE_PATTERN.fullmatch(os.path.basename(logfile))
             assert matching, f'Log file {logfile!r}\'s name not match with pattern {_LOG_FILE_PATTERN.pattern}.'
 
--- animetimm/utils/__init__.py
+++ animetimm/utils/__init__.py
@@ -2,3 +2,4 @@ from .cli import GLOBAL_CONTEXT_SETTINGS, print_version, parse_key_value
 from .md import markdown_to_df
 from .parallel import parallel_call
 from .profile import torch_model_profile
+from .tensorboard import is_tensorboard_log_empty
