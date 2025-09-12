32300504557c6310ecd5236b8b6e808baa1a65e8
HansBug <hansbug@buaa.edu.cn>
2025-09-12 13:36:47 +0800

    dev(narugo): x

--- animetimm/multilabel/normalize.py
+++ animetimm/multilabel/normalize.py
@@ -80,6 +80,12 @@ def load_dataloader(repo_id: str, model_name: str = 'caformer_s36.sail_in22k_ft_
 
 if __name__ == '__main__':
     dataset, transform = load_dataloader(repo_id='animetimm/danbooru-wdtagger-v4-w640-ws-full')
+    for sample in dataset:
+        image = sample['json']['image']
+        print(image)
+        print(transform(image).shape)
+        quit()
+
     print(dataset)
     print(transform)
     # for x in enumerate(dataloader):
